8886d548dbad37db2c063dc14488a8495420f3ee
Krzysiek014 <krzysiek014@o2.pl>
2018-02-21 14:20:03 +0100

    Add -> calculateRadius function

--- src/main/java/pl/krzysiek014/Math/MathOperations.java
+++ src/main/java/pl/krzysiek014/Math/MathOperations.java
@@ -1,6 +1,7 @@
 package pl.krzysiek014.Math;
 
 import javafx.geometry.Point2D;
+import javafx.scene.shape.Arc;
 
 /**
  * Created by Krzysiek014 on 21.02.2018.
@@ -10,5 +11,9 @@ public class MathOperations {
         x = (r * Math.sin(angle*Math.PI/180.0))+ x;
         y = (r * Math.cos(angle*Math.PI/180.0))+ y;
         return new Point2D(x,y);
+    }
+
+    public double calculateRadius(double wordRadius, Arc a) {
+        return wordRadius-((wordRadius-Math.sqrt((wordRadius*wordRadius)-( a.getRadiusX()*a.getRadiusX()/4)))+a.getRadiusX()/2*Math.sqrt(3));
     }
 }
