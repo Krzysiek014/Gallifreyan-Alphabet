f45c9365d1a6589ecce4bd98401b83e87afffcec
Krzysiek014 <krzysiek014@o2.pl>
2018-02-21 13:06:52 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Krzysiek014 on 21.02.2018.
+ */
+public class Main extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
