a9cfe32ad945801de39cd615be497a5eb78e47b4
Krzysiek014 <krzysiek014@o2.pl>
2018-02-21 15:06:49 +0100

    Add -> handle checkButton actions and create letter T

--- src/main/java/pl/krzysiek014/Controllers/WindowController.java
+++ src/main/java/pl/krzysiek014/Controllers/WindowController.java
@@ -2,9 +2,12 @@ package pl.krzysiek014.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import pl.krzysiek014.Letter.HalfOfCircle;
 import pl.krzysiek014.Math.MathOperations;
 
 /**
@@ -13,11 +16,37 @@ import pl.krzysiek014.Math.MathOperations;
 public class WindowController {
     @FXML
     AnchorPane window;
+    @FXML
+    Button checkButton;
+    @FXML
+    TextField textField;
     public void initialize(){
         MathOperations oper = new MathOperations();
         Circle word = new Circle(300,300,200, Color.WHITE);
         word.setStroke(Color.BLACK);
         window.getChildren().addAll(word);
+
+        checkButton.setOnAction(e->{
+            window.getChildren().remove(3,window.getChildren().size());
+            String[] list = textField.getText().split("");
+            double radiusInner = Math.pow(0.9,list.length-1);
+            for(int i =0, count = 0;i<360;i+=360/list.length,count++){
+                switch (list[count].toUpperCase()){
+                    case "B":
+                        break;
+                    case "J":
+                        break;
+                    case "T":
+                        Point2D start = oper.getPoint(i-(radiusInner*30),word.getCenterX(),word.getCenterY(),word.getRadius());
+                        Point2D end = oper.getPoint(i+(radiusInner*30),word.getCenterX(),word.getCenterY(),word.getRadius());
+                        window.getChildren().add(new HalfOfCircle(start.getX(),start.getY(),end.getX(),end.getY()));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
     }
 
 }
