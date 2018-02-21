c7ea8b768f5fb872b4a7cb80372a635919eae6bc
Krzysiek014 <krzysiek014@o2.pl>
2018-02-21 13:34:04 +0100

    Add -> getPoint() function to get point from circle

--- src/main/java/pl/krzysiek014/Controllers/WindowController.java
+++ src/main/java/pl/krzysiek014/Controllers/WindowController.java
@@ -1,6 +1,7 @@
 package pl.krzysiek014.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -15,5 +16,11 @@ public class WindowController {
         Circle word = new Circle(300,300,200, Color.WHITE);
         word.setStroke(Color.BLACK);
         window.getChildren().addAll(word);
+    }
+
+    private Point2D getPoint(double angle, double x, double y, double r){
+        x = (r * Math.sin(angle*Math.PI/180.0))+ x;
+        y = (r * Math.cos(angle*Math.PI/180.0))+ y;
+        return new Point2D(x,y);
     }
 }
