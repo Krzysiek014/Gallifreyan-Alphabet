99aedfae4f92b1287da39e5c1139c2e61d1244dd
Krzysiek014 <krzysiek014@o2.pl>
2018-02-21 22:34:51 +0100

    Add -> getInfo function

--- src/main/java/pl/krzysiek014/Letter/HalfOfCircle.java
+++ src/main/java/pl/krzysiek014/Letter/HalfOfCircle.java
@@ -8,10 +8,14 @@ import javafx.scene.shape.Path;
  * Created by Krzysiek014 on 21.02.2018.
  */
 public class HalfOfCircle extends Path{
-    public HalfOfCircle(double startX, double startY, double encX, double endY){
-        MoveTo start = new MoveTo(startX,startY);
 
-        ArcTo arcTo = new ArcTo();
+    private ArcTo arcTo;
+    MoveTo start;
+
+    public HalfOfCircle(double startX, double startY, double encX, double endY){
+        start = new MoveTo(startX,startY);
+
+        arcTo = new ArcTo();
         arcTo.setRadiusX(1);
         arcTo.setRadiusY(1);
         arcTo.setX(encX);
@@ -19,5 +23,13 @@ public class HalfOfCircle extends Path{
         arcTo.setSweepFlag(true);
 
         this.getElements().addAll(start,arcTo);
+    }
+
+    public double[] getInfo(){
+        double centerX = Math.abs((start.getX()+arcTo.getX())/2);
+        double centerY = Math.abs((start.getY()+arcTo.getY())/2);
+        double diffX =(centerX-start.getX())*(centerX-start.getX());
+        double diffY =(centerY-start.getY())*(centerY-start.getY());
+        return new double[]{centerX,centerY,diffX,diffY};
     }
 }
