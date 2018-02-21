2fe727c396026a2cf73df79ac99a3aaec7276832
Krzysiek014 <krzysiek014@o2.pl>
2018-02-21 14:20:43 +0100

    Add -> AlmostCircle class

---
+++ src/main/java/pl/krzysiek014/Letter/AlmostCircle.java
@@ -0,0 +1,28 @@
+package pl.krzysiek014.Letter;
+
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.Circle;
+import pl.krzysiek014.Math.MathOperations;
+
+/**
+ * Created by Krzysiek014 on 21.02.2018.
+ */
+public class AlmostCircle extends Arc {
+    public AlmostCircle(double angle, Circle word){
+        MathOperations oper = new MathOperations();
+
+        this.setRadiusX(100);
+        this.setRadiusY(100);
+
+        Point2D p = oper.getPoint(angle,word.getCenterX(),word.getCenterY(),oper.calculateRadius(word.getRadius(),this));
+        this.setCenterX(p.getX());
+        this.setCenterY(p.getY());
+
+        this.setLength(300f);
+        this.setStartAngle(angle-60f);
+        this.setFill(Color.WHITE);
+        this.setStroke(Color.BLACK);
+    }
+}
