43251117ae97cb7564831a592a1d5af664bbdcf9
Krzysiek014 <krzysiek014@o2.pl>
2018-02-21 16:32:05 +0100

    Add -> LinesAndDots can handle circle letters with dots

--- src/main/java/pl/krzysiek014/Letter/LinesAndDots.java
+++ src/main/java/pl/krzysiek014/Letter/LinesAndDots.java
@@ -13,13 +13,15 @@ import pl.krzysiek014.Math.MathOperations;
 public class LinesAndDots{
 
     Line[] lines;
+    Circle[] dots;
 
     public LinesAndDots(String what, int howMany, Circle c, double angle){
         switch (what){
             case "lines":
-                lines = makeLines(c.getCenterX(),c.getCenterY(),c.getRadius(),howMany, angle);
+                this.lines = makeLines(c.getCenterX(),c.getCenterY(),c.getRadius(),howMany, angle);
                 break;
             case "dots":
+                this.dots = makeDots(c.getCenterX(),c.getCenterY(),c.getRadius(),howMany, angle);
                 break;
         }
     }
@@ -60,13 +62,35 @@ public class LinesAndDots{
         return new Line[0];
     }
 
-    private void makeDots(double centerX, double centerY, double radius, double howMany){
-//        if(howMany==1){
-//
-//        }
+    private Circle[] makeDots(double centerX, double centerY, double radius, double howMany, double angle){
+        MathOperations oper = new MathOperations();
+        if(howMany==1){
+            Point2D start = oper.getPoint(180+angle,centerX,centerY,radius-20);
+            Circle c = new Circle(start.getX(),start.getY(),radius*0.1);
+            return new Circle[]{c};
+        }else if(howMany==2){
+            Point2D start1 = oper.getPoint(150+angle,centerX,centerY,radius-20);
+            Point2D start2 = oper.getPoint(210+angle,centerX,centerY,radius-20);
+            Circle c1 = new Circle(start1.getX(),start1.getY(),10);
+            Circle c2 = new Circle(start2.getX(),start2.getY(),10);
+            return new Circle[]{c1,c2};
+        }else if(howMany==3){
+            Point2D start1 = oper.getPoint(180+angle,centerX,centerY,radius-20);
+            Point2D start2 = oper.getPoint(210+angle,centerX,centerY,radius-20);
+            Point2D start3 = oper.getPoint(150+angle,centerX,centerY,radius-20);
+            Circle c1 = new Circle(start1.getX(),start1.getY(),radius*0.1);
+            Circle c2 = new Circle(start2.getX(),start2.getY(),radius*0.1);
+            Circle c3 = new Circle(start3.getX(),start3.getY(),radius*0.1);
+            return new Circle[]{c1,c2,c3};
+        }
+        return new Circle[0];
     }
 
     public Line[] getLines() {
         return lines;
+    }
+
+    public Circle[] getDots() {
+        return dots;
     }
 }
